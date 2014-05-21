06103ee9dd1cf3477fdf0b5ad4e8433165a6f0c5
TracyBean <trcmoscato@gmail.com>
2014-05-20 21:32:03 -0400

    first commit

---
+++ HelloWorld/HelloWorld/HelloWorld.h
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.h
+//  HelloWorld
+//
+//  Created by Tracy Moscato on 5/20/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HelloWorld : NSObject
+
+@end
---
+++ HelloWorld/HelloWorld/HelloWorld.m
@@ -0,0 +1,13 @@
+//
+//  HelloWorld.m
+//  HelloWorld
+//
+//  Created by Tracy Moscato on 5/20/14.
+//  Copyright (c) 2014 Appcoda. All rights reserved.
+//
+
+#import "HelloWorld.h"
+
+@implementation HelloWorld
+
+@end
